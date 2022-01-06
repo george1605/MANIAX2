2ba6cee26700c3fbe9567708f3c73429473441cd
george1605 <32997165+george1605@users.noreply.github.com>
2022-01-06 19:07:13 +0200

    Update lib.c

--- lib.c
+++ lib.c
@@ -1,5 +1,20 @@
 #pragma once
+#define true (char)1
+#define false (char)0
+#define NOP() asm("nop")
+#define HALT() asm("hlt")
+
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef char bool;
+typedef long vptr;
+
+int strlen(char* ptr){
+  if(ptr == 0) return 0;
+  int len = 0;
+  while(ptr[len] != 0){
+    len++;
+  }
+  return len;
+}
