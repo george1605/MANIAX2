1fbac31228d5270096b5ed3632d801afd8ff1939
george1605 <32997165+george1605@users.noreply.github.com>
2022-01-06 18:52:33 +0200

    Update io.h

--- io.h
+++ io.h
@@ -1,1 +1,38 @@
 #pragma once
+#include "lib.c"
+
+void outb(uint16_t port, uint8_t val)
+{
+    asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
+}
+
+void outw(uint16_t port, uint16_t val)
+{
+    asm volatile("outw %w0, %w1" : : "a" (val), "Nd" (port));
+}
+
+void outl(uint16_t port, uint32_t val)
+{
+    asm volatile("outl %0, %w1" : : "a" (val), "Nd" (port));
+}
+
+uint8_t inb(uint16_t port)
+{
+    uint8_t data;
+    asm volatile("inb %w1, %b0" : "=a" (data) : "Nd" (port));
+    return data;
+}
+
+uint16_t inw(uint16_t port)
+{
+    uint16_t data;
+    asm volatile("inw %w1, %w0" : "=a" (data) : "Nd" (port));
+    return data;
+}
+
+uint32_t inl(uint16_t port)
+{
+    uint32_t data;
+    asm volatile("inl %w1, %0" : "=a" (data) : "Nd" (port));
+    return data;
+}
